ed6008a9893b336ee9cb9e22824b6f8f6d94666f
ncampb02 <ncampb02@g.uafs.edu>
2015-03-15 11:29:46 -0500

    Added name to groupMembers.java

--- CapstoneProject-TheArc/src/groupMembers.java
+++ CapstoneProject-TheArc/src/groupMembers.java
@@ -6,6 +6,7 @@ public class groupMembers
     {
       //Zackary Moore
     	// just let you guys know Kenny is here.
+    	// Noah here. Gonna start messing with that index page today.
     }
 
   }
