75402e769abd0b67c2ea459e7d6c0e77527fc917
EvanBurkes <eburke00@g.uafs.edu>
2015-04-09 17:16:22 -0500

    Update GroupHomeDAO.java

--- CapstoneProject-TheArc/src/data/GroupHomeDAO.java
+++ CapstoneProject-TheArc/src/data/GroupHomeDAO.java
@@ -17,5 +17,34 @@ public class GroupHomeDAO {
 	 	GroupHome grouphome;
 	 	PreparedStatement statement=null;
 		String preparedSQL = "SELECT * FROM ARC_GroupHome;";
+		
+		
+	 try{
+	    	connection = ConnectionDAO.getCon();
+	    	statement = connection.prepareStatement(preparedSQL);
+			ResultSet rs = statement.executeQuery();
+			while(rs.next()){
+				grouphome = new GroupHome();
+				grouphome.setGRP_ID(rs.getString("GRP_ID"));
+				grouphome.setGRP_Name(rs.getString("GRP_Name"));
+				grouphome.setGRP_Add1(rs.getString("GRP_Add1"));
+				grouphome.setGRP_Add2(rs.getString("GRP_Add2"));
+				grouphome.setGRP_City(rs.getString("GRP_City"));
+				grouphome.setGRP_State(rs.getString("GRP_State"));
+				grouphome.setGRP_Zip(rs.getString("GRP_Zip"));
+			}	
+			rs.close();		
+			statement.close();
+			connection.close();
+		}
+	catch (SQLException ex){
+			System.out.println("Error: " + ex);
+			System.out.println("Query: " + statement.toString());
+		}
+		return grouphomes;
+	}
+	
+	
+	
 
 }
