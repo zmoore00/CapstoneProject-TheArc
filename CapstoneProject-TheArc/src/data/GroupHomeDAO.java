766f2a5b9ca29ad1045c4ac83ca6b98407afe663
EvanBurkes <eburke00@g.uafs.edu>
2015-04-09 15:34:19 -0500

    Update GroupHomeDAO.java

--- CapstoneProject-TheArc/src/data/GroupHomeDAO.java
+++ CapstoneProject-TheArc/src/data/GroupHomeDAO.java
@@ -1,5 +1,21 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import beans.GroupHomes;
+
 public class GroupHomeDAO {
+  
+	private static Connection connection=null;
+	
+		public synchronized ArrayList<GroupHome> getGroupHomes() {
+	 	ArrayList<GroupHome> grouphomes = new ArrayList<GroupHome>();
+	 	GroupHome grouphome;
+	 	PreparedStatement statement=null;
+		String preparedSQL = "SELECT * FROM ARC_GroupHome;";
 
 }
