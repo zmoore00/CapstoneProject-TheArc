5b2fcb6f1aea48c611fc2c09b9c3b761d79a97a2
EvanBurkes <eburke00@g.uafs.edu>
2015-04-16 15:38:49 -0500

    Update GroupHomeDAO.java

--- CapstoneProject-TheArc/src/data/GroupHomeDAO.java
+++ CapstoneProject-TheArc/src/data/GroupHomeDAO.java
@@ -45,8 +45,75 @@ public class GroupHomeDAO {
 	}
 	
 	public synchronized int addGroupHome(GroupHome grouphome){
+		int status=0;
+		String preparedSQL = "INSERT INTO ARC_GroupHome(GRP_ID,GRP_Name,GRP_Add1,GRP_Add2,GRP_City,GRP_State,GRP_Zip) VALUES(?,?,?,?,?,?,?);";
+		PreparedStatement statement=null;
 		
+		try {
+			connection = ConnectionDAO.getCon();
+			statement = connection.prepareStatement(preparedSQL);
+			statement.setString(1, grouphome.getGRP_ID());
+			statement.setString(2, grouphome.getGRP_Name());
+			statement.setString(3, grouphome.getGRP_Add1());
+			statement.setString(4, grouphome.getGRP_Add2());
+			statement.setString(5, grouphome.getGRP_City());
+			statement.setString(6, grouphome.getGRP_State());
+			statement.setString(7, grouphome.getGRP_Zip());
+			
+			status = statement.executeUpdate();
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return status;
 	}
 	
-
+	public synchronized int removeGroupHome(String GRP_ID){
+		int status=0;
+		String preparedSQL = "Delete From ARC_GroupHome where GRP_ID = ?";
+		PreparedStatement statement=null;
+		
+		try {
+			connection = ConnectionDAO.getCon();
+			statement = connection.prepareStatement(preparedSQL);
+			statement.setString(1, GRP_ID);
+			status = statement.executeUpdate();
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return status;
+	}
+	
+	public synchronized static int updateGroupHome(GroupHome grouphome){
+		int status=0;
+		PreparedStatement statement=null;
+		String preparedSQL = "UPDATE ARC_GroupHome SET GRP_Name = ?,GRP_Add1 = ?,GRP_Add2 = ?,GRP_City = ?,GRP_State = ?,GRP_Zip = ?  WHERE GRP_ID = ?;";
+		
+		
+		try{
+	    		connection = ConnectionDAO.getCon();
+	    		statement = connection.prepareStatement(preparedSQL);
+			statement.setString(1, grouphome.getGRP_Name());
+			statement.setString(2, grouphome.getGRP_Add1());
+			statement.setString(3, grouphome.getGRP_Add2());
+			statement.setString(4, grouphome.getGRP_City());
+			statement.setString(5, grouphome.getGRP_State());
+			statement.setString(6, grouphome.getGRP_Zip());
+			statement.setString(7, GRP_ID);
+			
+			status = statement.executeUpdate();
+			statement.close();
+			connection.close();
+		}catch (SQLException ex){
+			System.out.println("Error: " + ex);
+			System.out.println("Query: " + statement.toString());
+		}	
+	    return status;
+	}
+	
+	
+			
 }
